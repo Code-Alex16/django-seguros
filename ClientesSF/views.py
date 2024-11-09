4ed7e7e626d2d7bfaec98a4faa771df3363a979f
Alexander Bastidas <xanderjorge@hotmail.com>
2024-11-09 11:55:45 -0500

    modificaciones

--- ClientesSF/views.py
+++ ClientesSF/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def helloWordl(request):
+    return render(request, 'hello.html')
+
+def clientes(request):
+    return render(request, 'Clientes.html')
