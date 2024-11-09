3f17b3a8d758024b3ee05578c1b8f3fe6554e693
Alexander Bastidas <xanderjorge@hotmail.com>
2024-11-09 11:57:04 -0500

    proyecto principal

--- Seguros/settings.py
+++ Seguros/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clientes.apps.ClientesConfig',
     'ClientesSF.apps.ClientessfConfig'
 ]
 
