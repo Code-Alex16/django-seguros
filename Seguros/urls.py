ab0429a6790041efe6baf1d48fa0d7d566d383e8
Alexander Bastidas <xanderjorge@hotmail.com>
2024-11-09 11:56:40 -0500

    urls

--- Seguros/urls.py
+++ Seguros/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ClientesSF import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', views.helloWordl)
 ]
