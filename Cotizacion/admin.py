8ad2f6fe7f3e1739227ff90b404a1a875b877e71
Alexander Bastidas <xanderjorge@hotmail.com>
2024-11-17 10:06:14 -0500

    mejoras

--- Cotizacion/admin.py
+++ Cotizacion/admin.py
@@ -2,4 +2,7 @@ from django.contrib import admin
 from .models import Cotizacion
 # Register your models here.
 
-admin.site.register(Cotizacion)
+@admin.register(Cotizacion)
+class CotizacionAdmin(admin.ModelAdmin):
+    list_display = ('customer', 'quote_option', 'quote_price', 'created_quote', 'updated_quote')
+    list_filter = ('quote_option',)
